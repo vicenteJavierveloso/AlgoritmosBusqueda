20980a53059914f1960390c7102fba272e353c48
ChewyDonnut <ignacioalejandro.vega@alumnos.ulagos.cl>
2023-06-22 18:19:45 -0400

    Update Arbol.py

--- Arbol.py
+++ Arbol.py
@@ -5,22 +5,23 @@ class arbol:
         self.derecho = None     #correspnde a un nodo derecho
         # inician en None por que se deben agregar los datos al iniciar el programa
 
-    def agregar_al_arbol(self, dato, nodo):
-        if dato < nodo.dato:    #si el dato es menor al dato del nodo(esto es recursivo) guardara el dato a la izquierda
-            if nodo.izquierdo is None: #si no existe
-                nodo.izquierdo = (dato)#guarda directamente al nodo izquierdo
+    def agregar_al_arbol(self, nuevodato, nodoactual): #nuevo dato es un valor que se ingresa en algun momento y puede variar
+        if nuevodato < nodoactual.dato:    #si el dato es menor al dato del nodo(esto es recursivo) guardara el dato a la izquierda
+            if nodoactual.izquierdo is None: #si no existe
+                nodoactual.izquierdo = (nuevodato)#guarda directamente al nodo izquierdo
             else: #si existe
-                self.agregar_al_arbol(dato, nodo.izquierdo)#recorre cada vez el nodo izquierdo hasta encontrarlo vacio
-        elif dato > nodo.dato:#lo mismo pero con el derecho
-            if nodo.derecho is None:#lo mismo pero con el derecho
-                nodo.derecho = (dato)#lo mismo pero con el derecho
+                self.agregar_al_arbol(nuevodato, nodoactual.izquierdo)#recorre cada vez el nodo izquierdo hasta encontrarlo vacio
+        elif nuevodato > nodoactual.dato:#lo mismo pero con el derecho
+            if nodoactual.derecho is None:#lo mismo pero con el derecho
+                nodoactual.derecho = (nuevodato)#lo mismo pero con el derecho
             else:
-                self.agregar_al_arbol(dato, nodo.derecho)#lo mismo pero con el derecho
+                self.agregar_al_arbol(nuevodato, nodoactual.derecho)#lo mismo pero con el derecho
         else:#por si existe ya el dato
             print("El dato ya existe en el árbol.")
 
 
-
+raiz=arbol(5)#crea una raiz del arbol
+raiz.agregar_al_arbol(5,raiz)
 def in_orden(self):
     pass
 #busqueda(no se si todos los tipos),in orden, postordern,preorden
