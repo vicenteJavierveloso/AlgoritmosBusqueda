40be7b054c4b3de3718153cd70a5e571a9795e7d
vicenteJavierveloso <vicentejavier.veloso@alumnos.ulagos.cl>
2023-06-23 10:31:27 -0400

    Primer avance

---
+++ Proyecto/Grafos/BusquedaEnProfundidad.py
@@ -0,0 +1,85 @@
+import networkx as nx
+import time
+
+#Funcion que una vez retornado el diccionario que contiene los padres de cada
+#Nodo en el grafo, realiza un camino y lo agrega a una lista
+def Camino(padres, elemento, camino=[]):
+    #padres sera un diccionario, elemento es el elemento a buscar o nodo objetivo
+    #camino sera una variable auxiliar que contendra el camino durante toda la recursion
+    #Si el elemento se encuentra en el diccionario padres
+    if elemento in padres:
+        #añadir a la lista auxiliar EL VALOR DEL ELEMENTO EN EL DICCIONARIO PADRES, este corresponde a su padre
+        camino.append(padres[elemento])
+        #si el valor es None, entonces ese nodo no tiene padres
+        #asi que se retorna la lista
+        if padres[elemento] == None:
+            return camino
+        #si el valor es distinto de None
+        #el elemento pasa a ser el padre del elemento anterior
+        #y se llama recursivamente en el mismo diccionario padres
+        #pasando el nuevo elemento y la lista auxiliar
+        else:    
+            elemento = padres[elemento]
+            Camino(padres,elemento,camino)
+    #Una vez que termina el algoritmo se retorna la lista con el camino
+    #se corta la lista en el ultimo elemento porque este siempre sera None
+    #ya que se llega a un nodo sin padre y la funcion solo lo añadira sin mas
+    return camino[:-1]
+
+
+#Funcion que implementa el algoritmo principal Busqueda en profundidad
+#se deben pasar varios parametros ya que se trata de un algoritmo recursivo por naturaleza
+#el grafo a trabajar, la raiz o nodo raiz, el elemento a buscar, los nodos encontrados, los padres de los nodos procesados y el tiempo
+#Que se guardara en una primera instancia apenas se ejecute el algoritmo por primera vez.
+def BEP(grafo:nx.Graph,raiz:str, elemento:str,encontrados = [], padres={}, t0 =0):
+    #si el tiempo es 0
+    #cambia la variable por primera vez, luego no la cambia mas,
+    #pero sera pasado a la siguiente llamada recursiva de la funcion
+    if t0 == 0:
+        t0 = time.perf_counter()
+    #Si la lista de nodos encontrados esta vacia en primera instancia
+    #se añade el nodo raiz a la lista
+    #ademas el diccionario padres se le añade una llave que indicara
+    #el padre del nodo raiz que esta siendo procesado
+    if encontrados == []:
+        encontrados = [raiz] #nodos encontrados
+        #Padres del nodo raiz
+        padres[raiz] = None
+    #
+    #por cada vecino de el nodo raiz actual, si no se encuentra en la lista de nodos
+    #Encontrados, añadirlo a la lista de nodos encontrados
+    for w in grafo.neighbors(raiz):
+        if w not in encontrados:
+            encontrados.append(w)
+            #Padres del vecino w del nodo raiz
+            #actualizar el padre de cada nodo vecino de la raiz actual, naturalmente
+            #sera la raiz actual
+            padres[w] = raiz
+            #procesar de inmediato el nodo vecino, pasandolo como una nueva raiz
+            #pasar tambien el grafo que se esta trabajando, el elemento que se sigue buscando para que no
+            #se pierda, el diccionario de padres y el tiempo 0 (t0) para que no se pierda tambien
+            BEP(grafo,w,elemento,encontrados, padres, t0)
+    #finalmente se retorna una lista con los datos de interes
+    #sacados del algoritmo
+    #el diccionario con padres, que sera procesado luego por la funcion Camino(), el elemento que se esta buscando,
+    #nuevamente para no perderlo y procesarlo en la siguiente funcion que entrega la informacion
+    #procedente del algoritmo de manera entendible (BEP_format())
+    #y finalmente el tiempo de ejecucion del algoritmo que tambien sera procesada en 
+    #BEP_format()
+    return [padres,elemento, time.perf_counter()-t0]
+
+#funcion que da formato y entrega de manera entendible los datos
+# obtenidos en la ejecucion del algoritmo
+def BEP_format(padres,elemento,tiempo):
+    #el diccionario padres es procesado con la funcion Camino()
+    camino = Camino(padres,elemento)
+    #se inserta el elemento que se estaba buscando en un principio para formar un camino
+    camino.insert(0,elemento)
+    #se invierte el orden de la lista para darle el formato
+    #de nodo origen -> nodo objetivo
+    camino.reverse()
+    #retorna la informacion y el tiempo
+    if elemento in padres:
+        return f"El camino para llegar al nodo {elemento} es {camino} Tiempo: {tiempo*1000}ms"
+    else:
+        return f"El nodo no se encuentra en el grafo Tiempo: {tiempo*1000}ms"
---
+++ Proyecto/Grafos/main.py
@@ -0,0 +1,75 @@
+from BusquedaEnAnchura import BEA
+from BusquedaEnProfundidad import BEP, BEP_format
+import networkx as nx
+import matplotlib.pyplot as pyl
+
+#Inicio de creacion del grafo de prueba
+ciudades = ["Osorno", "Valdivia", "Frutillar", "Puerto Varas", "Puerto Montt", "Los Muermos", "Lago Ranco", "Futrono", "Santiago",
+             "Concepcion", "Constitucion", "Arica", "La Serena", "Entre Lagos", "Iquique", "Antofagasta", "Valparaiso", "Viña del Mar", "Punta Arenas"]
+
+mapa = nx.Graph()
+
+mapa.add_nodes_from(ciudades)
+
+# Añade atributos a los nodos
+atributos = {"Osorno": {"numero": "1"}, "Valdivia": {"numero": "1"}, "Frutillar": {"numero": "1"}, "Puerto Varas": {"numero": "1"},
+            "Puerto Montt": {"numero": "1"}, "Los Muermos": {"numero": "1"}, "Lago Ranco": {"numero": "1"}, "Futrono": {"numero": "10"},
+            "Santiago": {"numero": "1"}, "Concepcion": {"numero": "1"}, "Constitucion": {"numero": "1"}, "Arica": {"numero": "1"},
+            "La Serena": {"numero": "1"}, "Entre Lagos": {"numero": "1"}, "Iquique": {"numero": "1"}, 
+            "Antofagasta": {"numero": "1"}, "Valparaiso": {"numero": "1"}, "Viña del Mar": {"numero": "1"}, "Punta Arenas": {"numero": "1"}}
+
+nx.set_node_attributes(mapa,atributos)
+
+mapa.add_edge("Osorno","Frutillar")
+mapa.add_edge("Frutillar","Puerto Varas")
+mapa.add_edge("Puerto Varas", "Puerto Montt")
+mapa.add_edge("Puerto Varas", "Los Muermos")
+mapa.add_edge("Puerto Montt", "Los Muermos")
+mapa.add_edge("Osorno","Lago Ranco")
+mapa.add_edge("Osorno", "Futrono")
+mapa.add_edge("Osorno", "Entre Lagos")
+mapa.add_edge("Futrono","Lago Ranco")
+mapa.add_edge("Santiago", "Concepcion")
+mapa.add_edge("Santiago", "Constitucion")
+mapa.add_edge("Santiago","Valparaiso")
+mapa.add_edge("Santiago","Entre Lagos")
+mapa.add_edge("La Serena","Santiago")
+mapa.add_edge("Frutillar","Punta Arenas")
+mapa.add_edge("Iquique","Lago Ranco")
+mapa.add_edge("Antofagasta","Punta Arenas")
+mapa.add_edge("Arica","Punta Arenas")
+mapa.add_edge("Punta Arenas","Viña del Mar")
+mapa.add_edge("Iquique","Antofagasta")
+mapa.add_edge("Puerto Varas","La Serena")
+mapa.add_edge("Constitucion","Osorno")
+mapa.add_edge("Frutillar","Santiago")
+mapa.add_edge("Arica","Puerto Montt")
+mapa.add_edge("Futrono","Punta Arenas")
+mapa.add_edge("Entre Lagos","Frutillar")
+mapa.add_edge("Valdivia", "Constitucion")
+
+#Fin creacion grafo de prueba
+
+#Busqueda en Anchura
+#Buscar algun atributo (llave) en los nodos del grafo que contenga el elemento especificado
+# comenzando desde el nodo dado
+#EJ busqueda en anchura
+#Se busca en el grafo mapa, a partir del nodo "Los Muermos", el nodo que contenga dentro de la llave "numero"
+#de su diccionario, el elemento "10"
+print(BEA(mapa,"Los Muermos", "numero", "10"))
+
+#Ej busqueda en profundidad
+#Se busca en el grafo mapa, el camino desde el nodo "Valdivia" a el nodo "Osorno"
+# se debe ejecutar una vez para evitar spamear el print ya que retorna algo cada vez que se llama
+#la funcion por ser recursiva por lo tanto se guarda el resultado final en una variable busqueda
+busqueda = BEP(mapa,"Valdivia","Peru")
+#se da formato usando la funcion BEP_format()
+#busqueda[0]: diccionario padres
+#busqueda[1]: elemento a buscar
+#busqueda[2]: el tiempo de ejecucion
+#la funcion deberia retornar #[Entre Lagos, Santiago, Constitucion,Valdivia] como camino
+print(BEP_format(busqueda[0],busqueda[1],busqueda[2]))
+
+
+nx.draw(mapa, with_labels=True)
+pyl.show()
