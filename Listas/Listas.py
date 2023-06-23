604bf974fcf340e264e8954c50b95d5c4f6f0250
feveja <128820930+feveja@users.noreply.github.com>
2023-06-22 21:01:49 -0400

    Update Listas.py

--- Listas/Listas.py
+++ Listas/Listas.py
@@ -1,27 +1,30 @@
+#Recorre la lista utilizando un bucle for y verifica si cada elemento es igual al elemento buscado.
+# Si encuentra una coincidencia, devuelve el índice correspondiente donde se encontró el elemento.
 def busqueda_lineal(lista, elemento):
     for i in range(len(lista)):
         if lista[i] == elemento:
-            return i  
-    return -1  
+            return i # El índice donde se encuentra el elemento
+    return -1  # Si el elemento no está en la lista
 
 def busqueda_binaria(lista, elemento):
-    lista_ordenada = sorted(lista)
-    print(
-        lista_ordenada
-    )
+    lista_ordenada = sorted(lista) # Ordenar la lista antes de la búsqueda binaria
+    print(lista_ordenada)
+    #Se establecen los índices inicio y fin para definir la sección actual en la que se está buscando.
     inicio = 0
     fin = len(lista) - 1
-
+    #El bucle while se ejecuta mientras inicio sea menor o igual a fin. En cada iteración
     while inicio <= fin:
-        medio = (inicio + fin) // 2
+        # se calcula el índice medio de la sección actual. Si el elemento buscado es igual al elemento en el índice medio, se retorna el índice.
+        medio = (inicio + fin) // 2 # Calcular el índice medio de la sección actual
         if lista_ordenada[medio] == elemento:
-            return medio  
+            return medio  # El elemento buscado se encuentra en el índice medio 
+        #Si el elemento buscado es mayor que el elemento en el índice medio, se actualiza el índice inicio para buscar en la mitad derecha de la sección actual.
         elif lista_ordenada[medio] < elemento:
-            inicio = medio + 1
+            inicio = medio + 1 # El elemento buscado está en la mitad derecha de la sección
+        #Si el elemento buscado es menor que el elemento en el índice medio, se actualiza el índice fin para buscar en la mitad izquierda de la sección actual.
         else:
-            fin = medio - 1
-
-    return -1  
+            fin = medio - 1  # El elemento buscado está en la mitad izquierda de la sección
+    return -1  # Si el elemento no está en la lista 
 
 # Ejemplo de uso con lista de cadenas
 lista_cadenas = ["manzana", "uva", "cereza", "banana"]
