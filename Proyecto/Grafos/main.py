baba3f91e8345fe5fed7509fb66becf18b6789fb
vicenteJavierveloso <vicentejavier.veloso@alumnos.ulagos.cl>
2023-07-07 20:08:08 -0400

    Demostracion manejo de archivos y uso del algoritmo

--- Proyecto/Grafos/main.py
+++ Proyecto/Grafos/main.py
@@ -10,5 +10,30 @@ ruta = f"{ruta}\grafos\grafo0.graphml"
 
 grafo = cargar_grafo(ruta)
 
+#Ejemplo correcto
+busqueda_en_profundidad = BEP(grafo,"Valdivia","Entre Lagos")
+print(BEP_format(busqueda_en_profundidad))
+
+#Ejemplo falla busqueda no encontrada
+#Esto esta fallando
+busqueda_en_profundidad = BEP(grafo, "Valdivia", "NoExiste")
+print(BEP_format(busqueda_en_profundidad))
+
+# #Ejemplo falla nodo raiz no existe
+busqueda_en_profundidad = BEP(grafo, "NoExiste", "Entre Lagos")
+print(BEP_format(busqueda_en_profundidad))
+
+#Ejemplo correcto
+print(BEA(grafo,"Valdivia","numero","10"))
+
+#Ejemplo falla nodo raiz no existe
+print(BEA(grafo,"NoExiste","numero","10"))
+
+#Ejemplo falla llave o atributo no existe en el nodo
+print(BEA(grafo,"Valdivia","NoExiste", "10"))
+
+#Ejemplo falla elemento no existe 
+print(BEA(grafo, "Valdivia","numero","999"))
+
 nx.draw(grafo, with_labels=True)
 pyl.show()
